89258eca107abeac8917e7f76db7574d3a784b00
Mario Torres <scaramuzzini@gmail.com>
2025-04-15 12:04:05 -0400

    Projeto starlink

---
+++ src/Starlink.jsx
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function Starlink() {
+
+    const [totalDocs,setTotalDocs] = useState(0);
+    const [satelites, setSatelites] = useState([]);
+
+    useEffect(() => {
+        const fetchSatelites = async () => {
+            const response = await axios.post(
+                'https://api.spacexdata.com/v4/starlink/query',
+                {
+                    "query": {},
+                    "options": {
+                        limit: 100
+                    }
+                }
+            );
+            console.log(response.data);
+            setTotalDocs(response.data.totalDocs);
+            setSatelites(response.data.docs);
+        }
+        fetchSatelites();
+    }, []);
+
+    return (
+        <div>
+            <h1>Lista de Satelites ({totalDocs})</h1>
+            <ul>
+                {
+                    satelites.map((sat) => (
+                        <li>{sat.spaceTrack.OBJECT_NAME}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Starlink

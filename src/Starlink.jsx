5a1980c14c6ffb6e2ecaf5bf6f056eced0a4fdda
Mario Torres <scaramuzzini@gmail.com>
2025-04-16 10:49:44 -0400

    marcadores adicionados

--- src/Starlink.jsx
+++ src/Starlink.jsx
@@ -2,8 +2,9 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
-import { useMap } from 'react-leaflet/hooks'
 import 'leaflet/dist/leaflet.css'
+import { Marker } from 'react-leaflet/Marker';
+import { Popup } from 'react-leaflet/Popup'
 
 function Starlink() {
 
@@ -28,17 +29,40 @@ function Starlink() {
         fetchSatelites();
     }, []);
 
-    const position = [51.505, -0.09]
+    const ocean = [-3.092652536502263, -60.01849591940468]
+    const lanche = [-3.1230428200783815, -60.01331785839022]
 
     return (
         <div>
             <h1>Lista de Satelites ({totalDocs})</h1>
-            <MapContainer center={position} zoom={13} scrollWheelZoom={false} 
+            <MapContainer center={ocean} zoom={2} scrollWheelZoom={false} 
             style={{height: '70vh'}} >
                 <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
+                <Marker position={lanche}>
+                </Marker>
+
+                {
+                    satelites
+                        .filter((sat) => sat.latitude && sat.longitude)
+                        .map((sat) => (
+                        <Marker position={[sat.latitude,sat.longitude]}>
+                            <Popup>
+                                Nome: {sat.spaceTrack.OBJECT_NAME} <br/>
+                            </Popup>
+                        </Marker>
+                    ))
+                }
+
+                <Marker position={ocean}>
+                    <Popup>
+                        Samsung OCEAN Manaus <br/>
+                        Avenida Darcy Vargas, 1200 <br/>
+                        Recomendo prof. Cleto Leal.
+                    </Popup>
+                </Marker>
             </MapContainer>
             <ul>
                 {
